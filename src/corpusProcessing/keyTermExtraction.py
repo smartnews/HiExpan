9038b51e77e7147d9447b4276a736d2f8de0427e
ericzhouh <ericzhouh@hotmail.com>
2020-11-06 18:24:45 -0800

    simplify keyTermExtraction to only calculate stats

--- src/corpusProcessing/keyTermExtraction.py
+++ src/corpusProcessing/keyTermExtraction.py
@@ -40,19 +40,23 @@ def main(corpusName):
     outputEntity2IDFile = '../../data/' + corpusName + '/intermediate/entity2id.txt'
     outputEntity2SurfaceNameFile = '../../data/' + corpusName + '/intermediate/entity2surface_names.txt'
     outputEntity2FreqFile = '../../data/' + corpusName + '/intermediate/entity2freq.txt'
-    outputSentenceJsonFile = '../../data/' + corpusName + '/intermediate/sentences.json'
+#    outputSentenceJsonFile = '../../data/' + corpusName + '/intermediate/sentences.json'
 
     cnt = 0
     entity2id = {}
     entity2surface_names = {}
     entity2freq = defaultdict(int)
     with tqdm(total=os.path.getsize(inputFile)) as pbar:
-        with open(inputFile, "r") as fin, open(outputSentenceJsonFile, "w") as fout:
+        with open(inputFile, "r") as fin:
             #for line in tqdm(fin, total=get_num_lines(inputFile), desc="Extract Key Terms from Corpus"):
             for line in fin:
                 pbar.update(len(line.encode('utf-8')))
                 line = line.strip()
-                sentence = json.loads(line)
+                try:
+                    sentence = json.loads(line)
+                except ValueError as e:
+                    print(e)
+                    continue
                 lemma_list = sentence["lemma"]
                 ems_new = []
                 for mention in sentence["entityMentions"]:
@@ -68,15 +72,6 @@ def main(corpusName):
 
                     entity2freq[lemma_signature] += 1
                     entity2surface_names[lemma_signature][entity] += 1
-
-                    mention["entityId"] = entity2id[lemma_signature]
-                    ems_new.append(mention)
-
-                # remove duplicates in ems_new
-                ems_new = deduplicate(ems_new)
-
-                sentence["entityMentions"] = ems_new
-                fout.write(json.dumps(sentence)+"\n")
 
     with open(outputEntity2IDFile, "w") as fout:
         for ele in sorted(entity2id.items(), key = lambda x:(x[0], x[1])):
